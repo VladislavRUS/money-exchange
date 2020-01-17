981b3bc098825c9ffe08fa3a38b2c36ac3465fbc
Vladislav Kurochkin <vlad555.95@mail.ru>
2020-01-17 09:50:23 +0400

    Initial build subtree commit

---
+++ build/precache-manifest.11083ff50b653a71ee3d14403944adbd.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4183d51dd86ac89e87b0d17363b8eff0",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3e26ecb67e1938a51f45",
+    "url": "./static/css/4.117ce29f.chunk.css"
+  },
+  {
+    "revision": "0adfe29289ac00285fc8",
+    "url": "./static/css/main.e8579178.chunk.css"
+  },
+  {
+    "revision": "ba6cc69e02a8b1e86633",
+    "url": "./static/js/0.7cc67bb7.chunk.js"
+  },
+  {
+    "revision": "de869485329f446bacbd",
+    "url": "./static/js/1.e8822198.chunk.js"
+  },
+  {
+    "revision": "777403680615ed8aaf1f5be0a6f35d1c",
+    "url": "./static/js/1.e8822198.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0a6b83cc10fd6cbeb844",
+    "url": "./static/js/10.e9c2d446.chunk.js"
+  },
+  {
+    "revision": "1fce415e2cfff775f376",
+    "url": "./static/js/11.2d9e636d.chunk.js"
+  },
+  {
+    "revision": "3e26ecb67e1938a51f45",
+    "url": "./static/js/4.472c9764.chunk.js"
+  },
+  {
+    "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
+    "url": "./static/js/4.472c9764.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2339bbb396a8bc01647d",
+    "url": "./static/js/5.8212d44a.chunk.js"
+  },
+  {
+    "revision": "688a70bb6bed283eb0a1",
+    "url": "./static/js/6.77cbb523.chunk.js"
+  },
+  {
+    "revision": "72e1df86ec8d975db04f",
+    "url": "./static/js/7.1949cbb3.chunk.js"
+  },
+  {
+    "revision": "c644f8065b23ae4935ca",
+    "url": "./static/js/8.2b009074.chunk.js"
+  },
+  {
+    "revision": "c4da788d24185e721b56",
+    "url": "./static/js/9.6d2c53a0.chunk.js"
+  },
+  {
+    "revision": "0adfe29289ac00285fc8",
+    "url": "./static/js/main.d9b7ab50.chunk.js"
+  },
+  {
+    "revision": "a9287f4e8185d3e5503c",
+    "url": "./static/js/runtime-main.a97a4528.js"
+  },
+  {
+    "revision": "543bc93b2e32281bad1ede21bb3afbdd",
+    "url": "./static/media/mc_symbol.543bc93b.svg"
+  },
+  {
+    "revision": "c1fc5ca7585dcee4cc3fab7b93c4db1f",
+    "url": "./static/media/user.c1fc5ca7.jpeg"
+  }
+]);
